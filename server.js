4f1dbb209d1e803e30d9b3ab6006a352beae5173
AMEZIANEMOHAMED <87206808+AMEZIANEMOHAMED@users.noreply.github.com>
2023-02-15 15:31:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,47 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
-dotenv.config();
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello friends! The API server is up and running. This API was built by Viraj.',
-    })
-});
+	res.status(200).send({
+		message: 'Hello from Fibonachi (Aleksin Danila Sergeevich)!'
+	})
+})
 
 app.post('/', async (req, res) => {
-    try {
-        const promt = req.body.promt;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${promt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-          })
-          
-          res.status(200).send({
-            bot: response.data.choices[0].text
-          })
+	try {
+		const prompt = req.body.prompt;
+		
+		const response = await openai.createCompletion({
+			model: "text-davinci-003",
+			prompt: `${prompt}`,
+			max_tokens: 4000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+			top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+			frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+			presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+		});
+		
+		res.status(200).send({
+			bot: response.data.choices[0].text
+		});
+		
+	} catch (error) {
+		console.error(error)
+		res.status(500).send(error || 'Something went wrong');
+	}
+})
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({error});
-    }
-});
-
-app.listen(3000, () => console.log('server is running on port http://localhost:3000'));
+app.listen(5000, () => console.log('AI server started'))
