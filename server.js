8a30eddc01bb757b784b41a1f40aeab5bc84ec36
AMEZIANEMOHAMED <87206808+AMEZIANEMOHAMED@users.noreply.github.com>
2023-02-15 16:01:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
 	try {
-		const prompt = req.body.prompt;
+		const promt = req.body.promt;
 		
 		const response = await openai.createCompletion({
 			model: "text-davinci-003",
-			prompt: `${prompt}`,
+			prompt: `${promt}`,
 			max_tokens: 4000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
 			top_p: 1, // alternative to sampling with temperature, called nucleus sampling
 			frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
